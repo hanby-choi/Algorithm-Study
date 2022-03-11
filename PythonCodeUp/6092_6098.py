90013d0f0300b40b74a37b4d9dff7445d62c5120
hanby <hanby.choi@ewhain.net>
2022-03-12 05:10:59 +0900

    코드업 파이썬 기초 100제 6092-6098

--- PythonCodeUp/6092_6098.py
+++ PythonCodeUp/6092_6098.py
@@ -1,5 +1,5 @@
 # 6092~6098 [기초-종합]
-
+'''
 # 6092 : [기초-리스트] 이상한 출석 번호 부르기1(설명)(py)
 n = int(input())
 a = list(map(int, input().split()))
@@ -25,3 +25,35 @@ for i in range(1, n):
 print(min)
 
 # 6095 : [기초-리스트] 바둑판에 흰 돌 놓기(설명)(py)
+d = [[0]*20 for i in range(20)]
+n = int(input())
+for i in range(n):
+    x, y = map(int, input().split())
+    d[x][y] = 1
+for i in range(1, 20):
+    for j in range(1, 20):
+        print(d[i][j], end=' ')
+    print() # 줄바꿈
+'''
+# 6096 : [기초-리스트] 바둑알 십자 뒤집기(py)
+a=[]
+for i in range(19):
+    a.append(list(map(int, input().split())))
+
+n = int(input())
+
+for i in range(n):
+    x, y = map(int, input().split())
+    for j in range(19):
+        if a[x-1][j] == 0:
+            a[x-1][j] = 1
+        else:
+            a[x-1][j] = 0
+        if a[j][y-1] == 0:
+            a[j][y-1] = 1
+        else:
+            a[j][y-1] = 0
+for i in range(19):
+    for j in range(19):
+        print(a[i][j], end=' ')
+    print() # 줄바꿈
