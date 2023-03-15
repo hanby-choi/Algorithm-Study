4c13026b3d1d06db10e826407634c9f1ac742344
Hanby Choi <hanby.choi@ewhain.net>
2023-03-15 16:27:32 +0900

    [다이나믹 프로래밍] 이취코 Part 2 남은 문제 풀이

--- 이취코/다이나믹 프로그래밍/money.py
+++ 이취코/다이나믹 프로그래밍/money.py
@@ -5,3 +5,22 @@ N, M = map(int, input().split())
 money = []
 for i in range(N):
     money.append(int(input().rstrip()))
+money.sort()
+d = [10001] * 10001
+for i in money:
+    d[i] = 1
+for i in range(1, M+1):
+    for j in money:
+        if (i - j) > 0:
+            d[i] = min(d[i], d[i-j] + 1)
+if d[M] == 10001:
+    print(-1)
+else:
+    print(d[M])
+"""
+d[0] = 0
+for i in range(N): # 모든 money에 대해
+    for j in range(money[i], M+1): # money[i]부터 M까지의 j원을 만드는 방법
+        if d[j-money[i]] != 10001: # i-k원을 만드는 방법이 존재하는 경우
+            d[j] = min(d[j], d[j - money[i] + 1])
+"""
