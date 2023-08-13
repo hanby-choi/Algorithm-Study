572ea0b881e0aa3ff0194ef25d555fbe32d20140
Hanby Choi <hanby.choi@ewhain.net>
2023-08-14 00:38:23 +0900

    [E-PPER] 크레인 인형뽑기 게임

---
+++ E-PPER 기출/하/64061.py
@@ -0,0 +1,17 @@
+# 크레인 인형뽑기 게임 - 스택
+def solution(board, moves):
+    length = len(board)
+    stack = []
+    answer = 0
+    for i in moves:
+        for j in range(length):
+            target = board[j][i-1]
+            if target != 0:
+                if stack and stack[-1] == target:
+                    stack.pop()
+                    answer += 2
+                else:
+                    stack.append(target)
+                board[j][i-1] = 0
+                break
+    return answer
