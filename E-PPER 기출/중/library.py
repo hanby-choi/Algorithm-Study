9c1d4078d444acf0dc8a610e54b1c18acf64bb2d
Hanby Choi <hanby.choi@ewhain.net>
2023-09-06 01:01:45 +0900

    [E-PPER] 도서관 예약

---
+++ E-PPER 기출/중/library.py
@@ -0,0 +1,21 @@
+# 도서관 예약: 그리디
+import sys
+input = sys.stdin.readline
+n = int(input())
+s = list(map(int, input().split()))
+e = list(map(int, input().split()))
+def solution(n, s, e):
+	student = [(s[i], e[i]) for i in range(n)]
+	student.sort(key = lambda x: (x[1], x[0]))
+	e1 = e2 = -1
+	ans = 0
+	for st in student:
+		if e1 <= st[0]:
+			e1 = st[1]
+			ans += 1
+		elif e2 <= st[0]:
+			e2 = e1
+			e1 = st[1]
+			ans += 1
+	return ans
+print(solution(n, s, e))
