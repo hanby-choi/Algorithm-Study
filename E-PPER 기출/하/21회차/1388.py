2b025b6f1236fabc8f9d90796acd644281368a25
Hanby Choi <hanby.choi@ewhain.net>
2023-09-13 01:30:43 +0900

    [E-PPER] 바닥 장식 코드 업데이트

--- E-PPER 기출/하/21회차/1388.py
+++ E-PPER 기출/하/21회차/1388.py
@@ -1,4 +1,4 @@
-# 바닥 장식: DFS
+"""# 바닥 장식: DFS
 import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
@@ -20,6 +20,65 @@ def dfs(x, y, last):
 cnt = 0
 for i in range(n):
     for j in range(m):
-        if dfs(i, j, floor[i][j]):
-            cnt += 1
-print(cnt)
+        if floor[i][j] != 0:
+            if dfs(i, j, floor[i][j]):
+                cnt += 1
+print(cnt)"""
+"""
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+floor = []
+for i in range(n):
+    floor.append(list(input().rstrip()))
+def dfs(x, y, n, m, floor, visited):
+	visited[x][y] = True
+	if floor[x][y] == '-':
+		nx = x
+		ny = y + 1
+	elif floor[x][y] == '|':
+		nx = x + 1
+		ny = y
+	if 0 <= nx < n and 0 <= ny < m and floor[x][y] == floor[nx][ny] and not visited[nx][ny]:
+		dfs(nx, ny, n, m, floor, visited)
+
+def solution(n, m, floor):
+	visited = [[False for _ in range(m)] for _ in range(n)]
+	ans = 0
+	for i in range(n):
+		for j in range(m):
+			if not visited[i][j]:
+				dfs(i, j, n, m, floor, visited)
+				ans += 1
+	return ans
+print(solution(n, m, floor))
+"""
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+floor = []
+for i in range(n):
+    floor.append(list(input().rstrip()))
+
+def dfs(x, y, last, n, m, floor):
+    if x < 0 or x >= n or y < 0 or y >= m:
+        return False
+    if floor[x][y] == '-' == last:
+        floor[x][y] = 0
+        dfs(x, y+1, '-', n, m, floor)
+        return True
+    elif floor[x][y] == '|' == last:
+        floor[x][y] = 0
+        dfs(x+1, y, '|', n, m, floor)
+        return True
+    return False
+
+def solution(n, m, floor):
+    ans = 0
+    for i in range(n):
+        for j in range(m):
+            if floor[i][j] != 0:
+                if dfs(i, j, floor[i][j], n, m, floor):
+                    ans += 1
+    return ans
+print(solution(n, m, floor))
