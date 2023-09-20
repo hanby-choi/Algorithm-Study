3e449c969337b00e99f9cf0446249ab9ce5b6136
Hanby Choi <76518934+hanby-choi@users.noreply.github.com>
2023-09-20 23:49:07 +0900

    [Bronze I] Title: 약수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.py
+++ 백준/Bronze/1037. 약수/약수.py
@@ -3,6 +3,5 @@ input = sys.stdin.readline
 cnt = int(input())
 arr = list(map(int, input().split()))
 def findN(cnt, arr):
-	arr.sort()
-	return arr[0] * arr[-1]
+	return max(arr) * min(arr)
 print(findN(cnt, arr))
