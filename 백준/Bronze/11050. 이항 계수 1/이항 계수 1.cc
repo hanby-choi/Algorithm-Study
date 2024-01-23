ddb0f5a056f2b7eff0dbb5996a12a3813b0f2d0d
Hanby Choi <76518934+hanby-choi@users.noreply.github.com>
2024-01-23 17:45:31 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n, k;
+	cin >> n >> k;
+	int num1 = 1;
+	int num2 = 1;
+	for(int i=1; i<k+1; i++){
+		num1 *= n-i+1;
+		num2 *= i;
+	}
+	cout << num1/num2;
+}
