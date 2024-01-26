65441a88f0095b03bc75b7b97b57057ba5d9f992
Hanby Choi <76518934+hanby-choi@users.noreply.github.com>
2024-01-26 22:04:23 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 int main(){
+    ios_base::sync_with_stdio(false);	// 동기화 해제
+	cin.tie(NULL);	// 묶음 해제
     int n, x, num;
     cin >> n >> x;
     for(int i=0; i<n; i++){
