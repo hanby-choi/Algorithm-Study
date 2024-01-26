acca76dd7be4f50c8d906060b9e1f5968dc88f07
Hanby Choi <76518934+hanby-choi@users.noreply.github.com>
2024-01-26 22:13:26 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << "|\\_/|" << '\n' << "|q p|   /}" << '\n' << "( 0 )\"\"\"\\" << '\n' << "|\"^\"`    |" << '\n' << "||_/=\\\\__|";
+}
