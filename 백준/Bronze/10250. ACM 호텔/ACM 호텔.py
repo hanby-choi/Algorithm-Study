f570a1f04d01012dda77ede8a2d172e40b5275f6
Hanby Choi <76518934+hanby-choi@users.noreply.github.com>
2024-01-27 16:21:29 +0900

    [Bronze III] Title: ACM 호텔, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,25 +1,16 @@
 import sys
 input = sys.stdin.readline
 
-def calcFloor(h, w, n):
-	ans = ''
-	# 층수 계산
-	remain = n % h
-	if remain == 0:
-		ans += str(h)
-		num = n // h
-	else:
-		ans += str(remain)
-		num = n // h + 1
-	# 호수 계산
-	if num <= 9:
-		ans += '0' + str(num)
-	else:
-		ans += str(num)
-	return ans
+def calcFloor(h, n):
+    floor = n % h
+    room_line = (n // h) + 1
+    if floor == 0:
+        floor = h
+        room_line -= 1
+    return floor * 100 + room_line
 
 t = int(input())
 while t > 0:
 	h, w, n = map(int, input().split())
-	print(calcFloor(h, w, n))
+	print(calcFloor(h, n))
 	t -= 1
