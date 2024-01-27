a3f02688d13b3989dfadc1c39c3bf3fac69a950c
Hanby Choi <76518934+hanby-choi@users.noreply.github.com>
2024-01-27 16:32:01 +0900

    [Bronze III] Title: 알람 시계, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,12 +1,10 @@
 import sys
 input = sys.stdin.readline
 h, m = map(int, input().split())
-if (m - 45) >= 0:
-	m -= 45
-else:
+if m < 45:
 	if h == 0:
 		h = 23
 	else:
 		h -= 1
-	m = m - 45 + 60 
-print(h, m)
+	m += 60
+print(h, m - 45)
