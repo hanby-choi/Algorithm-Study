cf43d54e1d8e94f3d81b0263ae2ba1043c7ba78b
Hanby Choi <76518934+hanby-choi@users.noreply.github.com>
2024-02-09 00:15:41 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1864 ms, Memory: 98732 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,26 +1,26 @@
-import sys, heapq
+import sys
+from collections import deque
 input = sys.stdin.readline
 
-def dijkstra(start):
-	q = []
-	heapq.heappush(q, (0, start))
+def BFS(start):
+	queue = deque([start])
 	distance[start] = 0
-	while q:
-		dist, now = heapq.heappop(q)
-		if distance[now] >= dist:
-			for i in graph[now]:
-				cost = dist + i[1]
-				if cost < distance[i[0]]:
-					distance[i[0]] = cost
-					heapq.heappush(q, (cost, i[0]))
+	while queue:
+		v = queue.popleft()
+		for i in graph[v]:
+			cost = distance[v] + 1
+			if distance[i] > cost:
+				distance[i] = cost
+				queue.append(i)
 
+INF = 1e5
 n, m, k, x = map(int, input().split())
 graph = [[] for _ in range(n+1)]
 for _ in range(m):
 	a, b = map(int, input().split())
-	graph[a].append((b, 1))
-distance = [1e5] * (n+1)
-dijkstra(x)
+	graph[a].append(b)
+distance = [INF] * (n+1)
+BFS(x)
 
 is_answer = False
 for i in range(1, n+1):
