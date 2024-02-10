fa150902e6749870f34518e631cb7a1c018f4feb
Hanby Choi <76518934+hanby-choi@users.noreply.github.com>
2024-02-10 23:22:54 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 240 ms, Memory: 49208 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline 
+n, m = map(int, input().split())
+dic = {}
+for _ in range(n):
+    url, pw = input().split()
+    dic[url] = pw
+for _ in range(m):
+    print(dic[input().rstrip()])
