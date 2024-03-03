b2429d7d2dbdf50b15e31f7616545384238063a7
Hanby Choi <76518934+hanby-choi@users.noreply.github.com>
2024-03-03 23:45:17 +0900

    [Silver II] Title: 에디터, Time: 564 ms, Memory: 40952 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+left = list(input().rstrip())
+right = []
+t = int(input())
+while t:
+	cmd = input().rstrip()
+	if cmd == 'L' and left:
+		right.append(left.pop())
+	elif cmd == 'D' and right:
+		left.append(right.pop())
+	elif cmd == 'B' and left:
+		left.pop()
+	elif cmd[0] == 'P':
+		left.append(cmd[2])
+	t -= 1
+ans = left + right[::-1]
+for a in ans:
+	print(a, end='')
