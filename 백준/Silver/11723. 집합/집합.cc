65ab4240a1b68b5c01aa8399ff8a57a5c373d606
Hanby Choi <76518934+hanby-choi@users.noreply.github.com>
2024-03-15 16:17:14 +0900

    [Silver V] Title: 집합, Time: 612 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -1,65 +1,53 @@
+// 11723: 집합 (실버5/비트마스킹)
 #include <iostream>
-#include <string>
-
 using namespace std;
-typedef long long ll;
-const long long ONES = 2097150;
-
-ll set = 0;
-
-void printExistence(int num) {
-    if (set & ((ll)1 << num)) {
-        cout << 1 << '\n';
-    }
-    else {
-        cout << 0 << '\n';
-    }
-}
-
-void changeSet(string cmd) {
-    if (cmd == "all") {
-        set = ONES;
-    }
-    else if (cmd == "empty") {
-        set = (ll)0;
-    }
-}
-
-void findSet(string cmd, int n) {
-    if (cmd == "add") {
-        set |= ((ll)1 << n);
-    }
-    else if (cmd == "remove") {
-        set &= ~((ll)1 << n);
-    }
-    else if (cmd == "toggle") {
-        set ^= ((ll)1 << n);
-    }
-}
-
-
+/**[백준 11723] 집합
+/* - vector나 map으로 각 원소의 유무를 판단할 경우 시간초과 발생!
+/*   => 총 원소의 개수는 20. 32비트 크기 int의 각 bit에 원소의 유무를 저장
+/* - value의 i번째 bit가 1이면: 현재 집합에 i 존재 O
+/* - value의 i번째 bit가 1이면: 현재 집합에 i 존재 X
+ */
 int main() {
-    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int n, num;
-    string cmd;
 
-    cin >> n;
+    int m, x, value = 0;
+    string op;
+    cin >> m;
+    while (m--) {
+        cin >> op;
 
-    while (n--) {
-        cin >> cmd;
-
-        if (cmd == "check"){ 
-            cin >> num;
-            printExistence(num);
+				// x 피연산자가 입력되지 않는 연산자: all, empty
+        if (op == "all") {
+            for (int i = 1; i <= 20; i++) {
+                value = value | (1 << i);
+            }
         }
-        else if (cmd == "all" || cmd =="empty") changeSet(cmd);
+        else if (op == "empty") {
+            value = 0;
+        }
+
+				// x 피연산자가 입력되는 연산자: add, remove, check, toggle
         else {
-            cin >> num;
-            findSet(cmd, num);
+            cin >> x;
+            if (op == "add") {
+                value = value | (1 << x);
+            }
+            else if (op == "remove") {
+                value = value & ~(1 << x);
+            }
+            else if (op == "check") {
+                if (value & (1 << x)) {
+                    cout << "1\n";
+                } else {
+                    cout << "0\n";
+                }
+            }
+            else if (op == "toggle") {
+                value = value ^ (1 << x);
+            }
         }
-        //cout << set << "\n";
     }
-
+    return 0;
 }
